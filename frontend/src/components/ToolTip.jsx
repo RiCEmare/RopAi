eb339756fb0a97bd87c231eecf3caa9c9106c330
RiCEmare <rikace.panta@gmail.com>
2025-01-22 12:51:24 +0545

    fix: improved tooltip positioning

--- frontend/src/components/ToolTip.jsx
+++ frontend/src/components/ToolTip.jsx
@@ -11,6 +11,7 @@ import {
   useRole,
   useInteractions,
   FloatingPortal,
+  useClientPoint,
 } from "@floating-ui/react";
 
 const Tooltip = ({ children, content }) => {
@@ -30,54 +31,58 @@ const Tooltip = ({ children, content }) => {
     ],
   });
 
+  
+
   const hover = useHover(context, { move: false });
   const focus = useFocus(context);
   const dismiss = useDismiss(context);
   const role = useRole(context, { role: "tooltip" });
+  const clientPoint = useClientPoint(context);
 
   const { getReferenceProps, getFloatingProps } = useInteractions([
     hover,
     focus,
     dismiss,
     role,
+    clientPoint,
   ]);
 
   return (
-    <>
-      <div
-        ref={refs.setReference}
-        {...getReferenceProps()}
-      >
-        {children}
-      </div>
-      <FloatingPortal>
-        {isOpen && (
-          <div
-            className="bg-black text-white border-2 border-green rounded-md font-zain font-light text-2xl flex-col p-3 pr-20 space-y-4"
-            ref={refs.setFloating}
-            style={floatingStyles}
-            {...getFloatingProps()}
-          >
-            <>
-              <p className="font-bold text-3xl">Ideal Platation Time:</p>
-              {content.plantation_time}
-            </>
-            <>
-              <p className="font-bold text-3xl">Best Planted Before:</p>
-              {content.planted_before}
-            </>
-            <>
-              <p className="font-bold text-3xl">Common Diseases:</p>
-              <ul className="list-disc pl-5">
-                {content.common_diseases.map((disease, index) => (
-                  <li key={index}>{disease}</li>
-                ))}
-              </ul>
-            </>
-          </div>
-        )}
-      </FloatingPortal>
-    </>
+      <>
+        <div
+          ref={refs.setReference}
+          {...getReferenceProps()}
+        >
+          {children}
+        </div>
+        <FloatingPortal>
+          {isOpen && (
+            <div
+              className="bg-black text-white border-2 border-green rounded-md font-zain font-light text-2xl flex-col p-3 pr-20 space-y-4"
+              ref={refs.setFloating}
+              style={floatingStyles}
+              {...getFloatingProps()}
+            >
+              <>
+                <p className="font-bold text-3xl">Ideal Platation Time:</p>
+                {content.plantation_time}
+              </>
+              <>
+                <p className="font-bold text-3xl">Best Planted Before:</p>
+                {content.planted_before}
+              </>
+              <>
+                <p className="font-bold text-3xl">Common Diseases:</p>
+                <ul className="list-disc pl-5">
+                  {content.common_diseases.map((disease, index) => (
+                    <li key={index}>{disease}</li>
+                  ))}
+                </ul>
+              </>
+            </div>
+          )}
+        </FloatingPortal>
+      </>
   );
 };
 
