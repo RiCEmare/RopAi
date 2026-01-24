975350e3e8522bd070d071cd83f0ef8c40ec74a0
RiCEmare <rikace.panta@gmail.com>
2026-01-24 16:20:46 +0545

    fix: update BASE_URL to use environment variable for API base URL

--- frontend/src/utils/apiCalls.jsx
+++ frontend/src/utils/apiCalls.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getWeatherdata = async (position, month, apiKey = null) => {
 	console.log("Position:", position.lng);
