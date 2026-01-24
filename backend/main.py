f068232a17ac90ca2fb8baa5dde13a5e87db3b96
RiCEmare <rikace.panta@gmail.com>
2026-01-24 16:17:17 +0545

    fix: cors origin for render site

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",           # Local React dev server
+        "https://ropai.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
